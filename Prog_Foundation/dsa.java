607c0d76facf71bc4ce97a97f63b908d8853a23e
Utkarsh Ghanwat <utkarshghanwat45@gmail.com>
2026-08-12 10:11:09 +0530

    DSA Que

---
+++ Prog_Foundation/dsa.java
@@ -0,0 +1,26 @@
+package Prog_Foundation;
+
+class dsa {
+
+    public static int Largest_Element(int arr[]) {
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+        }
+
+        return max;
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = { 2, 3, 4, 1, 5, 7 };
+        
+        System.out.println(Largest_Element(arr));
+    }
+}
